8f107f41d677981308f23687c377f7b369f6cb89
Piotr Lewandowski <pwlew@amazon.com>
2021-03-21 15:47:37 +0100

    Add initial version of Network class
    - Added Network class with its basic methods
    - Added basic tests

---
+++ network_science/ns/__init__.py
@@ -0,0 +1,12 @@
+import tensornetwork as tn
+
+
+def compare_nodes(node_a: tn.Node, node_b: tn.Node) -> bool:
+    return node_a.tensor == node_b.tensor \
+           and node_a.name == node_b.name \
+           and node_a.edges == node_b.edges \
+           and node_a.shape == node_b.shape \
+           and node_a.axis_names == node_b.axis_names \
+           and node_a.dtype == node_b.dtype \
+           and node_a.sparse_shape == node_b.sparse_shape \
+           and node_a.__class__ == node_b.__class__
