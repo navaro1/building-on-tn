8f09fe21b22ef38a077ab9c32a5c77b47108d2ab
Piotr Lewandowski <piotr@petlew.com>
2021-03-27 06:14:39 +0100

    Ns/adjacency matrix (#2)
    * Working adjacency matrix
    
    Co-authored-by: Piotr Lewandowski <piotr@petlew.com>

--- network_science/ns/network.py
+++ network_science/ns/network.py
@@ -1,19 +1,45 @@
 import uuid
 
+import numpy as np
 import tensornetwork as tn
 
-from network_science.ns import compare_nodes
 
-
-# TODO:
-# - add tests for all methods
+# - improve string representation
 
 class DirectedNetwork:
     """Class representing Networks. Network is a collection of nodes joined by edges"""
 
-    def __init__(self) -> None:
+    def __init__(self, adjacency_matrix: np.ndarray = None) -> None:
         self._nodes = {}
         self._edges = {}
+        self._adjacency_matrix = adjacency_matrix
+        if adjacency_matrix is None or adjacency_matrix.shape[-1] == 0:
+            pass
+        else:
+            self._build_from_adj_matrix(adjacency_matrix)
+
+    def _build_from_adj_matrix(self, adjacency_matrix):
+        if not all(np.array(adjacency_matrix.shape) == adjacency_matrix.shape[0]):
+            raise ValueError(f"adjacency matrix needs to be square, but was {adjacency_matrix.shape}")
+        it = np.nditer(adjacency_matrix, flags=['multi_index'])
+        for value in it:
+            if value == 0:
+                for idx in it.multi_index:
+                    self.add_node_if_does_not_exist(0, str(idx + 1))  # shift is required to enumerate nodes from 1
+            elif all(np.array(it.multi_index) == it.multi_index[0]):  # diagonal
+                if value % 2 != 0:
+                    raise ValueError("Non even number on an diagonal")
+                node_idx = it.multi_index[0] + 1
+                self.add_node_if_does_not_exist(0, str(node_idx))
+                for times in range(value // 2):
+                    self.add_edge(node_idx, node_idx)
+            else:  # Not handling hypergraphs as of now
+                for idx in range(value):
+                    idx_from = str(it.multi_index[0] + 1)
+                    idx_to = str(it.multi_index[1] + 1)
+                    self.add_node_if_does_not_exist(0, idx_from)
+                    self.add_node_if_does_not_exist(0, idx_to)
+                    self.add_edge(idx_from, idx_to)
 
     @property
     def nodes(self):
@@ -23,14 +49,34 @@ class DirectedNetwork:
     def edges(self):
         return self._edges.copy()
 
+    @property
+    def adjacency_matrix(self):
+        # For now discard values
+        if self._adjacency_matrix is None:
+            size = len(self._nodes)
+            node_name_to_idx = {}
+            idx = 0
+            for node in self._nodes.keys():
+                node_name_to_idx[node] = idx
+                idx += 1
+            adj_matrix = np.zeros(shape=(size, size))  # only 2d
+            for node in self._nodes.keys():
+                if node in self._edges:
+                    for target_node in self._edges[node]:
+                        from_idx = node_name_to_idx[node]
+                        target_idx = node_name_to_idx[target_node]
+                        adj_matrix[from_idx][target_idx] = adj_matrix[from_idx][target_idx] + (
+                            2 if from_idx == target_idx else 1)
+            self._adjacency_matrix = adj_matrix
+        return self._adjacency_matrix
+
     def create_nodes_and_edge(self, from_value, to_value, from_name: str = None, to_name: str = None) -> None:
+        self.add_node_if_does_not_exist(from_value)
+        self.add_node_if_does_not_exist(to_value)
         str_from = from_name if from_name is not None else str(from_value)
         str_to = to_name if to_name is not None else str(to_value)
-        if str_from not in self._nodes:
-            self.add_node(from_value, str_from)
-        if str_to not in self._nodes:
-            self.add_node(to_value, str_to)
         if str_from not in self._edges or str_to not in self._edges[str_from]:
+            self._adjacency_matrix = None  # Refresh adj matrix on next retrieval
             self.add_edge(str_from, str_to)
 
     def add_node(self, node_value, node_name: str = None) -> None:
@@ -39,11 +85,19 @@ class DirectedNetwork:
             raise ValueError('Node: "%s" already exists' % actual_node_name)
 
         new_node = tn.Node(node_value, name=actual_node_name)
+        self._adjacency_matrix = None  # Refresh adj matrix on next retrieval
         self._nodes[actual_node_name] = new_node
+
+    def add_node_if_does_not_exist(self, node_value, node_name: str = None) -> None:
+        actual_node_name = node_name if node_name is not None else str(node_value)
+        if actual_node_name not in self._nodes:
+            new_node = tn.Node(node_value, name=actual_node_name)
+            self._nodes[actual_node_name] = new_node
 
     def add_node_unique(self, node_value) -> str:
         node_name = str(uuid.uuid4())
         self.add_node(node_value, node_name)
+        self._adjacency_matrix = None  # Refresh adj matrix on next retrieval
         return node_name
 
     def remove_node(self, node_name) -> bool:
@@ -52,6 +106,7 @@ class DirectedNetwork:
         if str_node_name in self._nodes:
             self._nodes.pop(str_node_name)
             result = True
+            self._adjacency_matrix = None  # Refresh adj matrix on next retrieval
             for key in self._edges.keys():
                 if key == str_node_name:
                     self._edges.pop(str_node_name)
@@ -67,6 +122,7 @@ class DirectedNetwork:
             raise ValueError('Node: "%s" does not exist' % str_from)
         if str_to not in self._nodes:
             raise ValueError('Node: "%s" does not exist' % to_node)
+        self._adjacency_matrix = None  # Refresh adj matrix on next retrieval
         if str_from not in self._edges:
             self._edges[str_from] = list(str_to)
         else:
@@ -83,6 +139,7 @@ class DirectedNetwork:
                 if len(current_edges) == 0:
                     self._edges.pop(str_from)
                 result = True
+                self._adjacency_matrix = None  # Refresh adj matrix on next retrieval
             except ValueError:
                 result = False
         return result
@@ -97,31 +154,59 @@ class DirectedNetwork:
         return self._edges == other.edges and self._compare_all_nodes(other)
 
     def _compare_all_nodes(self, other):
-        # Very inefficient
-        for key in self._nodes.keys():
-            if key not in other.nodes:
-                return False
-            self_node = self._nodes[key]
-            other_node = other.nodes[key]
-            if not compare_nodes(self_node, other_node):
-                return False
-        return True
+        return (self.adjacency_matrix == other.adjacency_matrix).all()
 
 
 class UndirectedNetwork(DirectedNetwork):
 
-    def create_nodes_and_edge(self, from_value, to_value, from_name: str = None, to_name: str = None) -> None:
-        super().create_nodes_and_edge(from_value, to_value, from_name, to_name)
-        super().add_edge(to_value, from_value)
+    def __init__(self, adjacency_matrix: np.ndarray = None) -> None:
+        if adjacency_matrix is not None and \
+                (not all(np.array(adjacency_matrix.shape) == adjacency_matrix.shape[0])
+                 or not (adjacency_matrix.transpose() == adjacency_matrix).all()):
+            raise ValueError("Undirected Network can have only symmetric adjacency matrix")
+        super().__init__(adjacency_matrix)
 
-    def add_node(self, node_value, node_name: str = None) -> None:
-        super().add_node(node_value, node_name)
+    def _build_from_adj_matrix(self, adjacency_matrix):
+        it = np.nditer(adjacency_matrix, flags=['multi_index'])
+        for value in it:
+            if value == 0:
+                for idx in it.multi_index:
+                    self.add_node_if_does_not_exist(0, str(idx + 1))  # shift is required to enumerate nodes from 1
+            elif all(np.array(it.multi_index) == it.multi_index[0]):  # diagonal
+                if value % 2 != 0:
+                    raise ValueError("Non even number on an diagonal")
+                node_idx = it.multi_index[0] + 1
+                self.add_node_if_does_not_exist(0, str(node_idx))
+                for times in range(value // 2):
+                    self.add_edge(node_idx, node_idx)
+            else:
+                # Not handling hypergraphs as of now
+                for times in range(value):
+                    idx_from = str(it.multi_index[0] + 1)
+                    idx_to = str(it.multi_index[1] + 1)
+                    self.add_node_if_does_not_exist(0, idx_from)
+                    self.add_node_if_does_not_exist(0, idx_to)
+                    super().add_edge(idx_from, idx_to)
 
-    def add_node_unique(self, node_value) -> str:
-        return super().add_node_unique(node_value)
-
-    def remove_node(self, node_name) -> bool:
-        return super().remove_node(node_name)
+    @property
+    def adjacency_matrix(self):
+        # For now discard values
+        if self._adjacency_matrix is None:
+            size = len(self._nodes)
+            node_name_to_idx = {}
+            idx = 0
+            for node in self._nodes.keys():
+                node_name_to_idx[node] = idx
+                idx += 1
+            adj_matrix = np.zeros(shape=(size, size))  # only 2d
+            for node in self._nodes.keys():
+                if node in self._edges:
+                    for target_node in self._edges[node]:
+                        from_idx = node_name_to_idx[node]
+                        target_idx = node_name_to_idx[target_node]
+                        adj_matrix[from_idx][target_idx] = adj_matrix[from_idx][target_idx] + 1
+            self._adjacency_matrix = adj_matrix
+        return self._adjacency_matrix
 
     def add_edge(self, from_node, to_node) -> None:
         super().add_edge(from_node, to_node)
@@ -132,3 +217,6 @@ class UndirectedNetwork(DirectedNetwork):
 
     def __str__(self) -> str:
         return super().__str__()
+
+    def __eq__(self, other):
+        return super().__eq__(other)
--- network_science/tests/test_directed_network.py
+++ network_science/tests/test_directed_network.py
@@ -1,17 +1,18 @@
 import copy
 
+import numpy as np
 import pytest
 import tensornetwork as tn
 
 import network_science.ns.network as n
-from network_science.ns import compare_nodes
+from network_science.ns import nodes_equal
 
 
 def test_directed_add_node_should_add_node():
     dn = n.DirectedNetwork()
     dn.add_node(1)
     assert len(dn.nodes) == 1
-    assert compare_nodes(dn.nodes['1'], tn.Node(1, name="1"))
+    assert nodes_equal(dn.nodes['1'], tn.Node(1, name="1"))
 
 
 def test_directed_add_node_should_throw_if_node_already_exist():
@@ -92,3 +93,77 @@ def test_add_node_unique_should_add_new_node_every_time():
     dn.add_node_unique(1)
     dn.add_node_unique(1)
     assert len(dn.nodes) == 3
+
+
+def test_network_should_remember_order_of_insertion():
+    dn = n.DirectedNetwork()
+    dn.add_node(3)
+    dn.add_node(2)
+    dn.add_node(5)
+    assert list(dn.nodes.keys()) == ['3', '2', '5']
+
+
+def test_network_should_remember_order_of_insertion_even_when_element_removed():
+    dn = n.DirectedNetwork()
+    dn.add_node(3)
+    dn.add_node(2)
+    dn.add_node(5)
+    dn.remove_node(2)
+    assert list(dn.nodes.keys()) == ['3', '5']
+
+
+def test_directed_network_should_throw_on_non_square_adj_matrix():
+    with pytest.raises(ValueError):
+        n.DirectedNetwork(np.array([[1, 2, 3], [3, 2, 1]]))
+
+
+def test_directed_network_should_throw_on_non_even_entry_in_diagonal():
+    with pytest.raises(ValueError):
+        n.DirectedNetwork(np.array([[1, 0], [0, 0]]))
+
+
+def test_should_create_directed_network_from_single_entry():
+    dn = n.DirectedNetwork(np.array([0]))
+    assert len(dn.nodes) == 1
+    assert nodes_equal(dn.nodes['1'], tn.Node(0, name="1"))
+    assert len(dn.edges) == 0
+
+
+def test_should_be_able_to_create_directed_network_from_adj_matrix():
+    adj_matrix = np.array(
+        [[0, 2, 0, 0, 1, 0],
+         [1, 0, 1, 1, 0, 0],
+         [0, 1, 4, 1, 1, 1],
+         [0, 1, 1, 0, 0, 0],
+         [1, 0, 1, 0, 0, 0],
+         [0, 0, 1, 0, 0, 2]])
+    dn_from_adj = n.DirectedNetwork(adj_matrix)
+
+    dn = n.DirectedNetwork()
+    for node in range(1, 7):
+        dn.add_node(0, str(node))
+    dn.add_edge("1", "2")
+    dn.add_edge("1", "2")
+    dn.add_edge("1", "5")
+
+    dn.add_edge("2", "1")
+    dn.add_edge("2", "3")
+    dn.add_edge("2", "4")
+
+    dn.add_edge("3", "2")
+    dn.add_edge("3", "3")
+    dn.add_edge("3", "3")
+    dn.add_edge("3", "4")
+    dn.add_edge("3", "5")
+    dn.add_edge("3", "6")
+
+    dn.add_edge("4", "2")
+    dn.add_edge("4", "3")
+
+    dn.add_edge("5", "1")
+    dn.add_edge("5", "3")
+
+    dn.add_edge("6", "3")
+    dn.add_edge("6", "6")
+    assert dn_from_adj == dn
+    assert (adj_matrix == dn.adjacency_matrix).all()
